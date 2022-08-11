f24c3354c17232939b3f5db1374d5c4b8a3927ab
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-11 13:15:22 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Main from "./components/Main";
 
 export default function App() {
   return (
-    <>
+    <div className="container">
       <Navbar />
       <Main />
-    </>
+    </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import logo from "../images/react-logo.png";
+import "../index.css"
 
 export default function Navbar() {
   return (
-    <>
-      <h1>The Navbar will go here</h1>
-    </>
+    <nav className="top">
+      <div className="logo">
+        <img src={logo} alt="react logo" className="react-icon" />
+        <h2>ReactFacts</h2>
+      </div>
+      <h3>React Course - Project 1</h3>
+    </nav>
   );
 }
