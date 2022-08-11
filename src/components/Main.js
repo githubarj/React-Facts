e5cabb86ad60f2e3b6a49b9a648a86e1e0b5a962
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-11 14:07:10 +0300

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,20 @@ export default function Main() {
     <div className="main">
       <h1>Fun facts about React</h1>
       <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k starts on GitHub</li>
-        <li>Is maintaned by Facebook</li>
-        <li>Powersthousands of enterprise apps, including
-          mobile apps
+        <li>
+          <span>Was first released in 2013</span>
+        </li>
+        <li>
+          <span>Was originally created by Jordan Walke</span>
+        </li>
+        <li>
+          <span>Has well over 100k starts on GitHub</span>
+        </li>
+        <li>
+          <span>Is maintaned by Facebook</span>
+        </li>
+        <li>
+          <span>Powersthousands of enterprise apps, including mobile apps</span>
         </li>
       </ul>
     </div>
