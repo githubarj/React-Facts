b024da21327a01b7f38bd7c371e26991abd51ea8
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-11 13:54:20 +0300

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "../index.css"
 
 export default function Main() {
   return (
-    <>
-      <h1>The Main Content will go here</h1>
-    </>
+    <div className="main">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on GitHub</li>
+        <li>Is maintaned by Facebook</li>
+        <li>Powersthousands of enterprise apps, including
+          mobile apps
+        </li>
+      </ul>
+    </div>
   );
 }
